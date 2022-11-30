ffc64bd1fd4ab50c08b49fdafec6da8f5cb24f61
Iñigo Taibo <itaibo@hey.com>
2022-11-30 22:55:08 +0100

    react-app-template

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import styles from './styles/App.module.css';
+
+export default function App() {
+	return (
+		<div className={styles.app}>
+			<div>Clean React App template</div>
+		</div>
+	);
+};
